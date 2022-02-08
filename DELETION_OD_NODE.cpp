42ed8d457be6c679fc619c9df85520ece56d59b8
Atharv Manoj Patil <93934533+AtharvPat@users.noreply.github.com>
2022-02-08 23:18:55 +0530

    Update DELETION_OD_NODE.cpp

--- DELETION_OD_NODE.cpp
+++ DELETION_OD_NODE.cpp
@@ -40,6 +40,34 @@ Node *takeinput()
     return head;
 }
 
+
+
+
+//deletion of node recursive
+
+// Node* deleteNodeRec(Node *head, int i) {
+  
+//     if(head==NULL ||i<0)
+//         return head;
+//     if(i==0)
+//     {
+//         Node *p=head;
+//         head=p->next;
+//         delete p;
+//         return head;
+//     }
+//     Node *x=deleteNodeRec(head->next,i-1);
+//     head->next=x;
+//     x=head;
+//     return head;
+// }
+
+
+
+
+
+
+
 Node *deleteNode(Node *head, int pos)
 {
     Node* temp = head;
